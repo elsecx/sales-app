df4943f06c68fa134cc8e8ab6a02097557f45cb7
elsecx <alwafielgin@gmail.com>
2025-08-19 02:30:45 +0700

    feat: preparing tabs for owner

--- app/(owner)/_layout.tsx
+++ app/(owner)/_layout.tsx
@@ -1,9 +1,14 @@
+import { View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
 import { useAuth } from "@/contexts/AuthContext";
-import { Stack, router } from "expo-router";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { FontAwesome } from "@expo/vector-icons";
+import { Link, router, Tabs } from "expo-router";
 import { useEffect } from "react";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, Pressable } from "react-native";
 
 export default function OwnerLayout() {
+  const colorScheme = useColorScheme() ?? "light";
   const { user } = useAuth();
 
   useEffect(() => {
@@ -22,5 +27,105 @@ export default function OwnerLayout() {
     );
   }
 
-  return <Stack screenOptions={{ headerShown: true }} />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: true,
+        tabBarActiveTintColor: Colors[colorScheme].primary.default,
+        tabBarStyle: {
+          backgroundColor: Colors[colorScheme].background,
+          borderTopWidth: 0.5,
+          borderTopColor: Colors[colorScheme].border.default,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Dashboard",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" size={size} color={color} />
+          ),
+          headerLeft: () => (
+            <Link href="/login" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="calendar"
+                    size={25}
+                    color={Colors[colorScheme].text.default}
+                    style={{ marginLeft: 15, marginRight: 10, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="sales"
+        options={{
+          title: "Penjualan",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="money" size={size} color={color} />
+          ),
+          headerLeft: () => (
+            <Link href="/login" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="calendar"
+                    size={25}
+                    color={Colors[colorScheme].text.default}
+                    style={{ marginLeft: 15, marginRight: 10, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+          headerRight: () => (
+            <Link href="/login" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="plus"
+                    size={25}
+                    color={Colors[colorScheme].text.default}
+                    style={{marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="products"
+        options={{
+          title: "Produk",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="cubes" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="employee"
+        options={{
+          title: "Pegawai",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="users" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="shops"
+        options={{
+          title: "Toko",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="shopping-basket" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
